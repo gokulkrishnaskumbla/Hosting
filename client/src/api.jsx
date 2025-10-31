67303b8e02a9bf66d99cbd213e6cacf80c2f19c5
gokulkrishnaskumbla <gokulkrishnaskumbla@gmail.com>
2025-10-31 21:46:20 +0530

    Change API base URL to production environment
    up

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:"http://localhost:8000/api/",withCredentials:true
+    baseURL:"https://hosting-1ebg.onrender.com",withCredentials:true
 })
 
 export default api
